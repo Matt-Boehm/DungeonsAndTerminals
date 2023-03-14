3c8556b953cdcb3e907ace71561848201ec2d32a
mboehm <matthew_boehm@pennunited.com>
2023-03-14 13:25:52 -0400

    Work in progress

--- DungeonsAndTerminals/Knight.cs
+++ DungeonsAndTerminals/Knight.cs
@@ -2,6 +2,7 @@
 {
     public void Method2()
     {
-        Console.WriteLine("You chose Option 2.");
+        Console.WriteLine("Terminal Master");
+        Console.Write("You picked the Knight! Nice choice! The Knight is very good at close combat and has good defence. Let's begin your journey!");
     }
 }
--- DungeonsAndTerminals/Wizard.cs
+++ DungeonsAndTerminals/Wizard.cs
@@ -1,10 +1,73 @@
-﻿using System.Collections.Generic;
-class Wizard
+﻿class Wizard
 {
     public void Method1()
     {
-        Console.WriteLine("You chose Option 1.");
+        Console.WriteLine("Terminal Master");
+        Console.WriteLine("You picked the Wizard! Nice choice! The Wizard has a veriety of skills including spellcasting, intelligence and long range attacks. " +
+            "Let's begin your journey!");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        break;
+        Console.WriteLine("Terminal Master");
+        Console.WriteLine("Welcome to TerminalVille, Wizard! Here you will experience some of the darkest and scariest adventures of your life!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
 
 
